ee71f55bb1ee884c6a5c5cd9288cdc30e839d179
Shem <shem.yu@tripsaas.com>
2020-11-03 08:18:01 +0800

    [Add] README.md自動生成程式

---
+++ bin/content.py
@@ -0,0 +1,40 @@
+import os
+import sys
+
+
+
+def generate():
+    os.chdir('..')
+    host_url = 'https://github.com/ShemYu/learning-resource/blob/master/'
+    white_folder_list = ['reading']
+    content_dict = {}
+
+    for f_name in os.listdir():
+        if f_name in ['reading']:
+            content_dict[f_name] = []
+            md_name_list = [md_name.split('.')[0]
+                            for md_name in os.listdir(f_name)]
+            git_url = 'https://github.com/ShemYu/learning-resource/blob/master'
+            md_url_list = ['{}/{}/{}'.format(git_url, f_name, md_name)
+                        for md_name in os.listdir(f_name)]
+            for i, md_name in enumerate(md_name_list):
+                content_line = '\t1. [{}]({})'.format(md_name, md_url_list[i])
+                content_dict[f_name].append(content_line)
+
+    content = '# Content\n\n'
+    for section in content_dict:
+        content += '- {}\n\n'.format(section)
+        content += '\n\n'.join(content_dict[section])
+        content += '\n\n'
+    
+    abstract = laoading_readme_abstract()
+    rdmd = open('README.md','w')
+    # rdmd.write(abstract+content)
+    print(abstract+content)
+
+def laoading_readme_abstract():
+    rdmd = open('README.md', 'r')
+    rdmd.containt = rdmd.read()
+    rdmd.abstract = rdmd.containt.split('# Content')[0]
+    return rdmd.abstract
+
