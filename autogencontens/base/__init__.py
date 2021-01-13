706d7b51f9a34a8a7e2a1d485c5cdbe5190afe26
Shem Yu <shemyu@wisers.com>
2021-01-13 11:54:01 +0800

    Add base_class

---
+++ autogencontens/base/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+__author__ = "Shem_Yu"
+
+from .base_class import Section, MarkdownReading
---
+++ autogencontens/base/base_class.py
@@ -0,0 +1,15 @@
+"""Define base class."""
+# -*- coding: utf-8 -*-
+__author__ = "Shem_Yu"
+
+
+class Section():
+    """Sections in contents."""
+    def __init__(self):
+        pass
+
+
+class MarkdownReading():
+    """Reading in contents"""
+    def __init__(self):
+        pass
