da192e920699254bc2234f2261278b96ff9541ea
Shem Yu <shemyu@wisers.com>
2021-02-05 09:00:34 +0800

    update path to base

--- autogencontents/base/__init__.py
+++ autogencontents/base/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 __author__ = "Shem_Yu"
 
-from .base_class import Section, MarkdownReading
+from .base_class import Section, Reading
--- autogencontents/transform/path_to_base_class.py
+++ autogencontents/transform/path_to_base_class.py
@@ -6,15 +6,17 @@ import os
 
 class ReadingLoader():
     """from path to Sections and MarkdownReading"""
-    def __init__(self, dirs:str = os.path.join("..", "reading"), reading_type:list = [".md"]):
-        self.path = dirs
-    def _walk(self):
+    def __init__(self, dirs:str = None, reading_type:list = [".md"]):
+        self.path = dirs if dirs else os.path.join(os.getcwd(), "reading")
+    def _walk_and_parse(self):
         """Walk path method."""
-        print(123)
-        for path in os.walk(self.path):
-            print(path)
+        print("Walk through {}".format(self.path))
+        return [i for i in os.walk(self.path)]
+    
+    def _parse(self, arg, dirnames, names):
+        print(names)
         
-    def _parse(self):
-        pass
     def get_structure(self):
-        self._walk()
+        raw_paths = self._walk()
+        self._parse(raw_paths)
+        
