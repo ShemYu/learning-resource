263e657b39810975be1ae7fb78f5845d2271f2e3
Shem Yu <shemyu@wisers.com>
2021-03-03 18:17:06 +0800

    Done walking and parsing

--- autogencontents/transform/path_to_base_class.py
+++ autogencontents/transform/path_to_base_class.py
@@ -1,34 +1,34 @@
 # -*- coding: utf-8 -*-
 __author__ = "Shem_Yu"
 
-from autogencontents.base.base_class import Path
 import os
 
+from autogencontents.base.base_class import Path
 
-class ReadingLoader():
+
+class ReadingLoader:
     """from path to Sections and MarkdownReading"""
-    def __init__(self, dirs:str = None, reading_type:list = [".md"]):
+
+    def __init__(self, dirs: str = None, reading_type: list = [".md"]):
         """Init.
 
         Args:
             dirs (str, optional): root to walk. Defaults to None.
             reading_type (list, optional): List of what to find. Defaults to [".md"].
-        """        
+        """
         self.path = dirs if dirs else os.path.join(os.getcwd(), "reading")
+
     def _walk_and_parse(self):
         """Walk path method."""
         print("Walk through {}".format(self.path))
-        path_list = []
+        Contents = ""
         for walk_result in os.walk(self.path):
-            path_list.append(Path(walk_result))
-        
-        self._parse(path_list)
-        return path_list
-    
-    def _parse(self, path_list):
-        for path in path_list:
-            print(path.files)
-        
+            p = Path(walk_result)
+            Contents += str(p.sec) + "\n"
+            Contents += "\n".join([str(r) for r in p.files])
+        return Contents
+
     def get_structure(self):
-        raw_paths = self._walk_and_parse()
-        
+        contents_str = self._walk_and_parse()
+        print(contents_str)
+        # Do update README.md
