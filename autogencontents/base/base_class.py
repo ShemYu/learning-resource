fb88b340bf6744302556d2f40b6947cb31e03ef5
Shem Yu <shemyu@wisers.com>
2021-03-03 10:46:39 +0800

    update Path, Section, Reading modules

--- autogencontents/base/base_class.py
+++ autogencontents/base/base_class.py
@@ -3,20 +3,29 @@
 __author__ = "Shem_Yu"
 
 
+import os
+
+
 class Path():
     """Path object return by os.walk"""
-    def __init__(self):
-        pass
-
-
-class Section(Path):
+    def __init__(self, walk_result):
+        self.arg = walk_result[0]
+        self.dirnames = walk_result[1]
+        self.files = walk_result[2]
+class Section():
     """Sections in contents."""
-    def __init__(self, path):
-        pass
+    def __init__(self, path, depth=0):
+        self.depth = depth
 
 
 class Reading(Path):
     """Reading in contents"""
-    def __init__(self):
-        pass
+    def __init__(self, sec:Section, name):
+        self.section = sec
+        self.name = name
+    def __str__(self) -> str:
+        str_type = "{}- {}".format("\t"*self.section.depth, self.name)
+        return str_type
+    def __repr__(self) -> str:
+        return self.__str__()
     
