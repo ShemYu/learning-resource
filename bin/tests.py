226e4d8c8ec322877a58bf7b7735fbb3fc4b860f
Shem Yu <shemyu@wisers.com>
2021-03-05 12:07:15 +0800

    Update tests

--- bin/tests.py
+++ bin/tests.py
@@ -3,14 +3,7 @@ import sys, os
 
 sys.path.append(os.getcwd())
 
-from autogencontents.transform import ReadingLoader
-
-
-def generate(root_dir='reading'):
-    """Get structure of specific reading with dict."""
-    rl = ReadingLoader()
-    rl.get_structure()
-
+from autogencontents.base import Readme
 
 if __name__ == "__main__":
-    generate()
+    Readme().auto_generate_contents()
